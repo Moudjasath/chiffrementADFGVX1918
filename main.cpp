ebe27b50d40b16606342b3604094dd67b9a403d2
Moudjasath <140810316+Moudjasath@users.noreply.github.com>
2023-12-06 12:05:00 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,55 +1,45 @@
 #include <iostream>
-#include <map>
-#include <string>
-#include <vector>
-#include <algorithm>
+using namespace std;
 
-// Tableau ADFGVX
-const std::map<char, std::string> adfgvxTable = {
-    {'A', "AA"}, {'D', "AD"}, {'F', "AF"}, {'G', "AG"}, {'V', "AV"}, {'X', "AX"},
-    {'B', "DA"}, {'C', "DD"}, {'E', "DF"}, {'H', "DG"}, {'I', "DV"}, {'K', "DX"},
-    {'L', "FA"}, {'M', "FD"}, {'N', "FF"}, {'O', "FG"}, {'P', "FV"}, {'Q', "FX"},
-    {'R', "GA"}, {'S', "GD"}, {'T', "GF"}, {'U', "GG"}, {'W', "GV"}, {'Y', "GX"},
-    {'Z', "VA"}
-};
-
-// Fonction pour chiffrement ADFGVX
-std::string chiffrementADFGVX(const std::string& message, const std::string& key) {
-    std::string cipherText;
-
-    // Création d'un vecteur pour stocker les paires ADFGVX
-    std::vector<std::pair<char, std::string>> adfgvxPairs;
-
-    // Remplissage du vecteur avec les valeurs de la table
-    for (char c : message) {
-        auto it = adfgvxTable.find(std::toupper(c));
-        if (it != adfgvxTable.end()) {
-            adfgvxPairs.push_back(std::make_pair(c, it->second));
-        }
+int modulo(int a, int b) {
+    int result = a % b;
+    if (result < 0) {
+        result += b;
     }
-
-    // Tri du vecteur par rapport à la clé
-    std::sort(adfgvxPairs.begin(), adfgvxPairs.end(), [&key](const std::pair<char, std::string>& a, const std::pair<char, std::string>& b) {
-        return key.find(a.second[0]) < key.find(b.second[0]);
-    });
-
-    // Construction du texte chiffré en fonction des paires triées
-    for (const auto& pair : adfgvxPairs) {
-        cipherText += pair.second;
-    }
-
-    return cipherText;
+    return result;
 }
 
+string encrypt(string message, int a, int b) {
+    string encryptedText = "";
+    for (char c : message) {
+        if (isalpha(c)) {
+            char encryptedChar = (toupper(c) - 'A') * a + b;
+            encryptedChar = modulo(encryptedChar, 26) + 'A';
+            encryptedText += encryptedChar;
+        } else {
+            encryptedText += c;
+        }
+    }
+    return encryptedText;
+}
+
+
 int main() {
-    std::string message = "LE TRAVAIL ET LA PERSEVERANCE "; // Message à chiffrer
-    std::string key = "1918"; // Clé de chiffrement
+    string message;
+    int a, b;
 
-    std::string cipherText = chiffrementADFGVX(message, key);
+    cout << "Entrez le message à chiffrer : ";
+    getline(cin, message);
 
-    std::cout << "Message clair : " << message << std::endl;
-    std::cout << "Clé de chiffrement : " << key << std::endl;
-    std::cout << "Message chiffré en ADFGVX : " << cipherText << std::endl;
+    cout << "Entrez la valeur de 'a' : ";
+    cin >> a;
+
+    cout << "Entrez la valeur de 'b': ";
+    cin >> b;
+
+    string encryptedMessage = encrypt(message, a, b);
+
+    cout << "Message chiffré : " << encryptedMessage << endl;
 
     return 0;
 }
